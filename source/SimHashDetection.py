a315781f5841b51fd30002b2a1d027131ce07714
DatTien <dat.nguyen.bk2006@gmail.com>
2025-11-03 15:21:41 +0700

    fix

---
+++ source/SimHashDetection.py
@@ -0,0 +1,29 @@
+from HSmodule import *
+from source.FaissSearch import FaissSearcher
+from source.Preprocessor import Shingling, TextEmbedder
+
+class SimHashDetection:
+    def __init__(self):
+        self.preprocessor = TextEmbedder()
+
+        self.hasher = SimHash()
+        self.outputDim = 128
+
+        self.searcher = FaissSearcher()
+        self.searcher.setDisFunc("cosine")
+        self.searcher.dim = self.outputDim
+
+    def detect(self, ListOfText : list ):
+        ListOfVecRecord = self.preprocessor(ListOfText)  
+
+        # cấu hình hasher cho phù hợp với kích thước vector đầu vào
+        sizeOfVector = len(ListOfVecRecord[0].vec)
+        self.hasher.setInOutput( sizeOfVector, self.outputDim)
+
+        listVecHashed = self.hasher.hash(ListOfVecRecord)
+
+        clusters = self.searcher.classify(listVecHashed)
+
+        # clustering đang là dạng list of list of VecRecord
+        
+        return clusters
