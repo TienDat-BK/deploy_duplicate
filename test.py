242be1adf826304c167e5eafeabb4c17c185e3c6
DatTien <dat.nguyen.bk2006@gmail.com>
2025-11-12 20:50:53 +0700

    upload linux binded

--- test.py
+++ test.py
@@ -2,10 +2,13 @@ import pandas as pd
 
 df = pd.read_csv("dataset/dataset_small.csv")
 
+no_rows = df[df["is_duplicate"] == "1"].head(20)
+yes_rows = df[df["is_duplicate"] == "0"].head(10)
+df = pd.concat([no_rows, yes_rows], ignore_index=True)
 df = df[["question1", "question2"]]
 
-df["question1"] = df["question1"] + df["question2"]
-df = df.drop(columns=["question2"])
+new_df = pd.DataFrame({"text" : pd.concat([df["question1"], df["question2"]], ignore_index=True)})
 
-df.to_csv("dataset/modified_dataset_question.csv", index=False)
+new_df.to_csv("dataset/modified_dataset_question_2.csv", index=False)
+
 
